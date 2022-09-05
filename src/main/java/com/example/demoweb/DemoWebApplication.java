e7ddb0cabb219392269d42b0261062faf963c258
vlaD <vlad.74022@gmail.com>
2022-09-05 10:58:37 +0500

    new port

--- src/main/java/com/example/demoweb/DemoWebApplication.java
+++ src/main/java/com/example/demoweb/DemoWebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoWebApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoWebApplication.class, args);
 	}
 
